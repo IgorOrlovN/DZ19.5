eb253ca5e3ca63e2e8426aec45c2b7f6bda274df
OrlovIgorN <orlikman@yandex.ru>
2021-04-21 17:55:50 +0300

    1

---
+++ DZ19.5/DZ19.5/DZ19.5.cpp
@@ -0,0 +1,41 @@
+﻿
+#include <iostream>
+
+class Animal
+{
+public:
+    void Voice()
+    {
+        std::cout << "Animal";
+    }
+};
+class Dog:public Animal
+{
+public:
+    void Voice()
+    {
+        std::cout << "Woof";
+    }
+};
+class Cat :public Animal
+{
+    void Voice()
+    {
+        std::cout << "Meow";
+    }
+};
+class Cow :public Animal
+{
+    void Voice()
+    {
+        std::cout << "Muuu";
+    }
+};
+
+int main()
+{
+    Dog* p = new Dog;
+    p->Voice();
+}
+
+
